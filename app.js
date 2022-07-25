f61f1d4d7bfc564a9a3c5ee2ab634d072f74353f
Yiğit <ytan2877@gmail.com>
2022-07-25 03:35:16 +0300

    Lesson02

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
 const express = require("express");
 
-
 const app = express();
 
-app.get("/", (req,res) => {
-    res.send("IndeX sayfası")
+// TEMPLATE ENGİNE
+app.set("view engine", "ejs");
+
+//MİDDLEWARES
+app.use(express.static('public'));
+
+//ROUTES
+app.get("/", (req, res) => {
+  res.status(200).render('index', {
+    pageName:"index"
+  })
+});
+
+app.get("/about", (req,res) => {
+  res.status(200).render('about' , {
+    pageName:'about'
+  })
 })
+
+app.get("/contact", (req, res) => {
+  res.status(200).render('contact')
+});
+
+app.get("/dashboard", (req, res) => {
+  res.status(200).render('dashboard')
+});
+
+app.get("/courses", (req, res) => {
+  res.status(200).render('courses')
+});
+
 const port = 3000;
 
 app.listen(port, () => {
-    console.log(`App started on Port ${port}`)
-})
+  console.log(`App started on Port ${port}`);
+});
